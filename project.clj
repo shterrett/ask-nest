04a6c754c2cd2abdb3dd7f9ba50fcfae8388bec9
Stuart Terrett <shterrett@gmail.com>
2015-05-06 21:51:29 -0400

    Reads data from nest

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.zenboxapp/taika "0.1.4"]])
+                 [http-kit "2.1.16"]
+                 [org.clojure/data.json "0.2.6"]])
--- src/ask_nest/core.clj
+++ src/ask_nest/core.clj
@@ -1,6 +1,27 @@
-(ns ask-nest.core)
+(ns ask-nest.core
+  (:require [org.httpkit.client :as http]
+            [clojure.data.json :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn nest-request [token]
+  @(http/get "https://developer-api.nest.com/" {:query-params {:auth token}}))
+
+(defn to-json [response-body]
+  (json/read-str response-body :key-fn keyword))
+
+(defn read-devices [token]
+  (let [{:keys [status headers body error]} (nest-request token)]
+    (if error
+      {:error true
+       :message error}
+      (into {:error false} (to-json body)))))
+
+(defn houses [nest-data]
+  (reduce (fn [hs h]
+            (into hs [[(:name h) {:postal-code (:postal_code h)
+                                  :country-code (:country_code h)
+                                  :thermostat-ids (:thermostats h)}]]))
+          {}
+          (vals (:structures nest-data))))
+
+(defn thermostats [nest-data]
+  (:thermostats (:devices nest-data)))
