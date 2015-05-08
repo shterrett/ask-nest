e6879e93f89e94060513590e6604deeea3fdd736
Stuart Terrett <shterrett@gmail.com>
2015-05-08 06:37:22 -0400

    Associates houses with thermostats

---
+++ test/data/nest_api_root.clj
@@ -0,0 +1,13 @@
+{:opts
+ {:url "https://developer-api.nest.com/",
+  :method :get,
+  :query-params
+  {:auth "nest-pin"}},
+ :body "{\"devices\":{\"thermostats\":{\"2-ShT4H3BvS8kWCWjPQZiHtAaHJ5cKch\":{\"humidity\":50,\"locale\":\"en-US\",\"temperature_scale\":\"F\",\"is_using_emergency_heat\":false,\"has_fan\":true,\"software_version\":\"4.3.1\",\"has_leaf\":true,\"device_id\":\"2-ShT4H3BvS8kWCWjPQZiHtAaHJ5cKch\",\"name\":\"Living Room\",\"can_heat\":true,\"can_cool\":true,\"hvac_mode\":\"heat\",\"target_temperature_c\":21.0,\"target_temperature_f\":70,\"target_temperature_high_c\":24.0,\"target_temperature_high_f\":75,\"target_temperature_low_c\":20.0,\"target_temperature_low_f\":68,\"ambient_temperature_c\":21.0,\"ambient_temperature_f\":70,\"away_temperature_high_c\":24.0,\"away_temperature_high_f\":76,\"away_temperature_low_c\":12.5,\"away_temperature_low_f\":55,\"structure_id\":\"dLlhu0oXCfCqxL-0_-rmq9FZcrpy_1_bXI8uB6aDJtxLhNmXjw2iJQ\",\"fan_timer_active\":false,\"name_long\":\"Living Room Thermostat\",\"is_online\":true}}},\"structures\":{\"dLlhu0oXCfCqxL-0_-rmq9FZcrpy_1_bXI8uB6aDJtxLhNmXjw2iJQ\":{\"name\":\"Home\",\"country_code\":\"US\",\"time_zone\":\"America/New_York\",\"away\":\"home\",\"thermostats\":[\"2-ShT4H3BvS8kWCWjPQZiHtAaHJ5cKch\"],\"structure_id\":\"dLlhu0oXCfCqxL-0_-rmq9FZcrpy_1_bXI8uB6aDJtxLhNmXjw2iJQ\"}},\"metadata\":{\"access_token\":\"nest-pin\",\"client_version\":1}}",
+ :headers
+ {:content-type "application/json; charset=UTF-8",
+  :content-length "1307",
+  :connection "close",
+  :cache-control "private, no-cache, max-age=0",
+  :access-control-allow-origin "*"},
+ :status 200}
