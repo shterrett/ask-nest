f94ab72aa53f0925d99f5c663ac4e3e0c42bb2c9
Stuart Terrett <shterrett@gmail.com>
2015-05-13 00:14:41 -0400

    Switches weather api
    Uses Dark Sky (api.forecast.io) instead of Open Weather Map
    (openweathermap.org/api) because Dark Sky provides hourly forecast for
    the current day, instead of only current conditions or daily forecast of
    the following day.

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
                  [http-kit.fake "0.2.1"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [environ "1.0.0"]]
+  :plugins [[environ "1.0.0"]])
+
