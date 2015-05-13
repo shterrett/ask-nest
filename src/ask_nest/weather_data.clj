31195037029d8088997c16b5731f668b74075f21
Stuart Terrett <shterrett@gmail.com>
2015-05-13 00:14:41 -0400

    Switches weather api
    Uses Dark Sky (api.forecast.io) instead of Open Weather Map
    (openweathermap.org/api) because Dark Sky provides hourly forecast for
    the current day, instead of only current conditions or daily forecast of
    the following day.

--- src/ask_nest/weather_data.clj
+++ src/ask_nest/weather_data.clj
@@ -1,27 +1,50 @@
 (ns ask-nest.weather-data
   (:require [org.httpkit.client :as http]
-            [ask-nest.data-shared :as data]))
+            [ask-nest.data-shared :as data]
+            [clojure.string :as string :only join]
+            [environ.core :refer [env]]))
 
-(def api-url "http://api.openweathermap.org/data/2.5/forecast/daily")
+(def api-url "https://api.forecast.io/forecast")
 
-(defn weather-request [city-id country-code]
-  (let [zip-param (format "%s,%s" city-id country-code)]
-    @(http/get api-url
-               {:query-params {:q zip-param
-                               :mode "json"
-                               :units "metric"
-                               :cnt 1}})))
+(defn weather-request [token latitude longitude]
+  (let [url (string/join "/" [api-url
+                              token
+                              (string/join "," [latitude longitude])])]
+    @(http/get url)))
 
-(defn read-weather [zip-code-or-name country-code]
-  (data/remote-request weather-request [zip-code-or-name country-code]))
+(defn read-weather [latitude longitude]
+  (let [token (env :weather-token)]
+    (data/remote-request weather-request [token latitude longitude])))
+
+(defn section-ly-data [data section]
+  (get-in data [section :data]))
+
+(defn average [data section sample-size value]
+  (/ (reduce (fn [total sample] (+ total (get sample value)))
+             0
+             (take sample-size (section-ly-data data section)))
+     sample-size))
+
+(defn max-val [data section sample-size value]
+  (value (apply max-key value (take sample-size (section-ly-data data section)))))
+
+(defn count-greater-than [data section sample-size value threshold]
+  (count (filter (fn [sample] (>= (get sample value) threshold))
+                 (take sample-size (section-ly-data data section)))))
+
+(defn major-value [data section sample-size value]
+  (let [value-counts (reduce (fn [counts sample]
+                               (assoc counts (get sample value) (+ 1 (get counts value 0))))
+                             {}
+                             (take sample-size (section-ly-data data section)))]
+    (key (apply max-key val (-> value-counts
+                              (dissoc nil)
+                              (assoc "none" 0))))))
 
 (defn forecast [weather-data]
-  (let [conditions (-> weather-data
-                      :list
-                      (first))
-        temp (:temp conditions)]
-    {:morning-temp (:morn temp)
-     :day-temp (:day temp)
-     :evening-temp (:eve temp)
-     :night-temp (:night temp)
-     :weather-type (map #(:id %) (:weather conditions))}))
+  (let [hourly-average (partial average weather-data :hourly 12)]
+    {:average-temp (hourly-average :temperature)
+     :average-apparent-temp (hourly-average :apparentTemperature)
+     :max-precip-prob (max-val weather-data :hourly 12 :precipProbability)
+     :hours-likely-precip (count-greater-than weather-data :hourly 12 :precipProbability 0.30)
+     :precip-type (major-value weather-data :hourly 12 :precipType)}))
--- test/ask_nest/weather_data_test.clj
+++ test/ask_nest/weather_data_test.clj
@@ -2,20 +2,28 @@
   (:require [clojure.test :refer :all]
             [ask-nest.weather-data :refer :all]
             [org.httpkit.client :as http]
-            [org.httpkit.fake :as fake-http]))
+            [org.httpkit.fake :as fake-http]
+            [environ.core :refer [env]]))
 
 (deftest weather_data
   (testing "data from weather api"
     (let [response (read-string (slurp "test/data/weather_api_boston.clj"))
-          zip-code "02215"
-          country-code "us"]
-      (fake-http/with-fake-http [api-url response]
+          latitude 42.3601
+          longitude -71.0589]
+      (fake-http/with-fake-http [(format "%s/%s/%s,%s"
+                                         api-url
+                                         (env :weather-token)
+                                         latitude
+                                         longitude)
+                                 response]
         (testing "fake data"
-          (is (= 200 (:status (weather-request zip-code country-code)))))
+          (is (= 200 (:status (weather-request (env :weather-token)
+                                               latitude
+                                               longitude)))))
         (testing "forecast"
-          (let [data (forecast (read-weather zip-code country-code))]
-            (is (= 16.4 (:morning-temp data)))
-            (is (= 20.84 (:day-temp data)))
-            (is (= 23.68 (:evening-temp data)))
-            (is (= 19.71 (:night-temp data)))
-            (is (= [500] (:weather-type data)))))))))
+          (let [data (forecast (read-weather latitude longitude))]
+            (is (> 0.1 (Math/abs (- 65.53 (:average-temp data)))))
+            (is (> 0.1 (Math/abs (- 65.82 (:average-apparent-temp data)))))
+            (is (= 0.43 (:max-precip-prob data)))
+            (is (= 3 (:hours-likely-precip data)))
+            (is (= "rain" (:precip-type data)))))))))
