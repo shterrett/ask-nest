83b2125d78d2e9f88497a82c06e220cac228c0a1
Stuart Terrett <shterrett@gmail.com>
2015-10-06 21:33:18 -0400

    pr comments

--- src/ask_nest/sms.clj
+++ src/ask_nest/sms.clj
@@ -6,14 +6,19 @@
 
 (def api-url "https://api.twilio.com/2010-04-01")
 
-(defn sms-sender [account token]
+(defn sms-sender [account token from]
   (let [url (string/join "/" [api-url
                               "Accounts"
                               account
                               "Messages.json"])]
-    (fn [to from body]
+    (fn [to body]
       @(http/post url {:form-params {:To to
                                      :From from
                                      :Body body
                                     }
                       :basic-auth [account token]}))))
+
+(defn get-sender []
+  (sms-sender (env :twilio-account)
+              (env :twilio-token)
+              (env :twilio-from)))
--- test/ask_nest/sms_test.clj
+++ test/ask_nest/sms_test.clj
@@ -6,13 +6,18 @@
             [environ.core :refer [env]]))
 
 (deftest sms
-  (deftest send-text-message
-    (let [request-url "https://api.twilio.com/2010-04-01/Accounts/accountid/Messages.json"
-          response (read-string (slurp "test/data/sms_response.clj"))
-          account "accountid"
-          token "secrettoken"]
+  (let [request-url "https://api.twilio.com/2010-04-01/Accounts/accountid/Messages.json"
+        response (read-string (slurp "test/data/sms_response.clj"))]
+    (deftest send-text-message
+      (let [account "accountid"
+            token "secrettoken"]
+        (fake-http/with-fake-http [request-url response]
+          (testing "successful text message send"
+            (let [sender (sms-sender account token "+11235556789")
+                  response (sender "123-456-7890" "Hello World")]
+              (is (= 201 (:status response))))))))
+    (deftest build-sms-sender
       (fake-http/with-fake-http [request-url response]
-        (testing "successful text message send"
-          (let [sender (sms-sender account token)
-                response (sender "123-456-7890" "+11235556789" "Hello World")]
+        (testing "uses env to configure sms-sender"
+          (let [response ((get-sender) "123-456-7890" "Hello World")]
             (is (= 201 (:status response)))))))))
