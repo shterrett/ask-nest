b39af59c2943c38718aad39bb3e1b0fff505fa21
Stuart Terrett <shterrett@gmail.com>
2015-10-09 17:44:26 -0400

    Build text message from forecast and decision

---
+++ src/ask_nest/message.clj
@@ -0,0 +1,23 @@
+(ns ask-nest.message
+  (:require [clojure.string :as string]))
+
+(defn weather-string [forecast]
+  (string/join ["It will be about "
+                (:average-temp forecast)
+                " degrees outside, with a "
+                (:max-precip-prob forecast)
+                "% chance of "
+                (name (:precip-type forecast))
+               "."]))
+
+(defn nest-string [decision]
+  (string/join ["Consider setting the thermostat to "
+                (name (:nest decision))
+                " and leaving the windows "
+                (name (:windows decision))
+                "."]))
+
+(defn text-message [forecast decision]
+  (string/join [(weather-string forecast)
+                " "
+                (nest-string decision)]))
